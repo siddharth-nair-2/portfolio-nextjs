6731f175d81bd7a269db0ea2a5c665e7f98c8198
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-25 00:53:53 -0400

    started archive

--- components/sections/Archive.tsx
+++ components/sections/Archive.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Archive = () => {
-  return <div>Archive</div>;
+  return (
+    <div className=" max-w-contentContainer mx-auto px-4 py-24">
+      <div className="w-full flex flex-col items-center">
+        <h2 className=" text-3xl font-semibold">Other Projects</h2>
+        <p className="text-sm text-textGreen">view the archive</p>
+      </div>
+    </div>
+  );
 };
 
 export default Archive;
