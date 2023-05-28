fcc22bc1ca98f87a9abf670a7ee73e2c8d090a20
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 21:51:07 -0400

    changes

--- app/get-repos.ts
+++ app/get-repos.ts
@@ -1,8 +1,2 @@
+import { Repo } from '@/types/Repo'
 import axios from 'axios'
-
-export async function getData() {
-  const { data } = await axios.get(
-    'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
-  )
-  return data
-}
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,25 @@ import RightBar from "@/components/RightBar";
 import About from "../components/sections/About";
 import Projects from "@/components/sections/Projects";
 import Archive from "@/components/sections/Archive";
-import { getData } from "./get-repos";
-import { Repo } from "@/types/Repo";
 import Contact from "@/components/sections/Contact";
 import Footer from "@/components/sections/Footer";
+import { Repo } from "@/types/Repo";
+
+async function getData(): Promise<Repo[]> {
+  const response = await fetch(
+    "https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc",
+    {
+      next: {
+        revalidate: 5,
+      },
+    }
+  );
+  const data = response.json();
+  return data;
+}
 
 export default async function Home() {
-  const tempRepos: Repo[] = await getData();
+  const tempRepos = await getData();
   return (
     <div className=" w-full h-[88vh] xl:flex items-center gap-20 justify-between">
       <LeftBar />
