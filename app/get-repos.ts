3467192ad6b99a5a88617c66e6749056733fb6f9
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 20:45:49 -0400

    s

--- app/get-repos.ts
+++ app/get-repos.ts
@@ -1,9 +1,6 @@
 export async function getData() {
   const res = await fetch(
     'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
-    {
-      cache: 'no-store',
-    },
   )
   const data = await res.json()
   return data
