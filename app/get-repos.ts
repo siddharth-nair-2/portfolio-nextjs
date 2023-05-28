ecfcb30a1a794f5fa87384124d324166b3ebfa74
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 20:31:49 -0400

    updated fetch to remove cache

--- app/get-repos.ts
+++ app/get-repos.ts
@@ -1,6 +1,9 @@
 export async function getData() {
   const res = await fetch(
     'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
+    {
+      cache: 'no-cache',
+    },
   )
   const data = await res.json()
   return data
