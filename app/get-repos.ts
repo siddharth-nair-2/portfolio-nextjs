43236411a4fe03c778a0dd8c8e43599127e299d3
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 21:17:26 -0400

    axios

--- app/get-repos.ts
+++ app/get-repos.ts
@@ -1,15 +1,8 @@
+import axios from 'axios'
+
 export async function getData() {
-  try {
-    const res = await fetch(
-      'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
-      {
-        cache: 'no-store',
-      },
-    )
-    const data = await res.json()
-    return data
-  } catch (error) {
-    if (error instanceof Error) console.log(error.message)
-    else console.log(String(error))
-  }
+  const { data } = await axios.get(
+    'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
+  )
+  return data
 }
