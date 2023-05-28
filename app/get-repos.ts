fdfc6d03ea62e8ef1155fd1c5665490ffc580f71
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 21:13:03 -0400

    trying with error catch

--- app/get-repos.ts
+++ app/get-repos.ts
@@ -1,8 +1,15 @@
-import axios from 'axios'
-
 export async function getData() {
-  const { data } = await axios.get(
-    'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
-  )
-  return data
+  try {
+    const res = await fetch(
+      'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
+      {
+        cache: 'no-store',
+      },
+    )
+    const data = await res.json()
+    return data
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message)
+    else console.log(String(error))
+  }
 }
