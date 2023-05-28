813b68c8b0946313cccb6454b5f8a5fd1e50783b
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 21:59:15 -0400

    updated

--- app/get-repos.ts
+++ app/get-repos.ts
@@ -1,2 +1,12 @@
 import { Repo } from '@/types/Repo'
-import axios from 'axios'
+
+export async function getData(): Promise<Repo[]> {
+  const response = await fetch(
+    'https://api.github.com/users/siddharth-nair-2/repos?sort=pushed&direction=desc',
+    {
+      cache: 'no-store',
+    },
+  )
+  const data = response.json()
+  return data
+}
